1abb3b4c56c8009697bdd6e4ae9c176dece94217
Lord-Whitesmith <matans844@gmail.com>
2022-12-05 21:03:40 +0200

    Initial Commit

---
+++ MachPortsServer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MachPortsServer
+//
+//  Created by matan on 05/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
